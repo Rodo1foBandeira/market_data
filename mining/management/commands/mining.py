953f735c8160fca1795937a6e3e92e65d9bc5537
Rodo1foBandeira <rodolfo_band@hotmail.com>
2019-05-08 06:21:56 -0300

    Ajustes

--- mining/management/commands/mining.py
+++ mining/management/commands/mining.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from mining.models import Trade
+from mining.models import Trade, Active
 import urllib.request, json 
 
 class Command(BaseCommand):
@@ -7,23 +7,24 @@ class Command(BaseCommand):
         url = "https://mdgateway04.easynvest.com.br/iwg/snapshot/?t=webgateway&c=5448062&q=WINM19"
         req = urllib.request.Request(url)
         trade = Trade()
-        while(1):
-            try:
-                with urllib.request.urlopen(req) as resp:
-                    data = json.loads(resp.read().decode())
+        try:
+            with urllib.request.urlopen(req) as resp:
+                data = json.loads(resp.read().decode())
 
-                    price = float(data['Value'][0]['Ps']['P'])
-                    business = int(data['Value'][0]['Ps']['TC'])
-                    tot_ctrcts_papers = int(data['Value'][0]['Ps']['TT'])
+                price = float(data['Value'][0]['Ps']['P'])
+                business = int(data['Value'][0]['Ps']['TC'])
+                tot_ctrcts_papers = int(data['Value'][0]['Ps']['TT'])
 
-                    if trade.price != price or trade.business != business or trade.tot_ctrcts_papers != tot_ctrcts_papers
-                        trade.id = None
-                        trade.active = 1
-                        time_stamp = data['Value'][0]['UT']
-                        trade.price = price
-                        trade.business = business
-                        trade.tot_ctrcts_papers = tot_ctrcts_papers
-                        trade.save()
+                if trade.price != price or trade.business != business or trade.tot_ctrcts_papers != tot_ctrcts_papers:
+                    trade.id = None
+                    trade.active = Active.objects.get(id=1)
+                    datetime = data['Value'][0]['UT']
+                    # 2019-05-07T18:01:23.932
+                    trade.time_stamp = datetime.datetime(int(datetime[0:4]), int(datetime[5:7]), int(datetime[8:10]), int(datetime[11:13]), int(datetime[14:16]), int(datetime[17:19]), int(datetime[20:24]))
+                    trade.price = price
+                    trade.business = business
+                    trade.tot_ctrcts_papers = tot_ctrcts_papers
+                    trade.save()
 
-            except urllib.error.URLError as e:
-                print(e.reason)
+        except urllib.error.URLError as e:
+            print(e.reason)
