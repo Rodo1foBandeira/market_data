9986ec06108ed77e2c4a951312e45ede370e30b9
Rodolfo Bandeira <rodolfo_band@hotmail.com>
2019-05-09 14:09:54 -0300

    Ajustes para minerar outros ativos

--- mining/management/commands/mining.py
+++ mining/management/commands/mining.py
@@ -3,34 +3,34 @@ from mining.models import Trade, Active
 import urllib.request, json, datetime, pytz
 
 class Command(BaseCommand):
+    trade['WINM19'] = Trade()
+    trade['WDOM19'] = Trade()
+    trade['WINM19'].active = Active.objects.get(id=1)
+    trade['WDOM19'].active = Active.objects.get(id=2)
+
     def handle(self, *args, **options):
         url = "https://mdgateway04.easynvest.com.br/iwg/snapshot/?t=webgateway&c=5448062&q=WINM19|WDOM19"
         req = urllib.request.Request(url)
-        trade = Trade()
-        trade2 = Trade()
-        active = Active.objects.get(id=1)
-        active2 = Active.objects.get(id=2)
+        
         while(True):
             try:
                 with urllib.request.urlopen(req) as resp:
                     data = json.loads(resp.read().decode())
-
-                    price = float(data['Value'][0]['Ps']['P'])
-                    business = int(data['Value'][0]['Ps']['TC'])
-                    tot_ctrcts_papers = int(data['Value'][0]['Ps']['TT'])
-
-                    if trade.price != price or trade.business != business or trade.tot_ctrcts_papers != tot_ctrcts_papers:
-                        trade.id = None
-                        trade.active = active
-                        datetime_buss = data['Value'][0]['UT']
-                        trade.datetime_buss = datetime.datetime(int(datetime_buss[0:4]), int(datetime_buss[5:7]), int(datetime_buss[8:10]), int(datetime_buss[11:13]), int(datetime_buss[14:16]), int(datetime_buss[17:19]), int(datetime_buss[20:24]), tzinfo=pytz.UTC)
-                        trade.price = price
-                        trade.business = business
-                        trade.tot_ctrcts_papers = tot_ctrcts_papers
-                        trade.save()
-                        #print('Preco: ' + data['Value'][0]['Ps']['P'])
-                        #print('Negocios: ' + data['Value'][0]['Ps']['TC'])
-                        #print('Contratos: ' + data['Value'][0]['Ps']['TT'])
-
+                    self.save(data['Value'][0])
+                    self.save(data['Value'][1])
             except urllib.error.URLError as e:
                 print(e.reason)
+
+    def save(self, response):
+        price = float(response['Ps']['P'])
+        business = int(response['Ps']['TC'])
+        tot_ctrcts_papers = int(response['Ps']['TT'])
+
+        if self.trade[response['S']].price != price or self.trade[response['S']].business != business or self.trade[response['S']].tot_ctrcts_papers != tot_ctrcts_papers:
+            self.trade[response['S']].id = None
+            datetime_buss = response['UT']
+            self.trade[response['S']].datetime_buss = datetime.datetime(int(datetime_buss[0:4]), int(datetime_buss[5:7]), int(datetime_buss[8:10]), int(datetime_buss[11:13]), int(datetime_buss[14:16]), int(datetime_buss[17:19]), int(datetime_buss[20:24]), tzinfo=pytz.UTC)
+            self.trade[response['S']].price = self.price
+            self.trade[response['S']].business = self.business
+            self.trade[response['S']].tot_ctrcts_papers = self.tot_ctrcts_papers
+            self.trade[response['S']].save()
