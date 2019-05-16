2e35bfa4bec06765920b9f00f0350973eb6821d9
Rodolfo de Almeida Bandeira <rodolfo_band@hotmail.com>
2019-05-16 18:38:06 -0300

    Implementa mineração de times trades

--- mining/management/commands/mining.py
+++ mining/management/commands/mining.py
@@ -31,9 +31,10 @@ class Command(BaseCommand):
     wdofut = 'WDO' + meses[hoje.month] + str(hoje.year)[2:4]
 
     trade = {}
-    trade[winfut] = Trade()
-    trade[wdofut] = Trade()
+    trade[winfut] = Trade.objects.filter(active_id=1).order_by('-id')[:1]
     trade[winfut].active = Active.objects.get(id=1)
+
+    trade[wdofut] = Trade.objects.filter(active_id=2).order_by('-id')[:1]
     trade[wdofut].active = Active.objects.get(id=2)
 
     def handle(self, *args, **options):
@@ -50,15 +51,29 @@ class Command(BaseCommand):
                 print(e.reason)
 
     def save(self, response):
-        price = float(response['Ps']['P'])
-        business = int(response['Ps']['TC'])
-        tot_ctrcts_papers = int(response['Ps']['TT'])
 
-        if self.trade[response['S']].price != price or self.trade[response['S']].business != business or self.trade[response['S']].tot_ctrcts_papers != tot_ctrcts_papers:
+        for item in response['Ts']:
+            item['DT'] = self.toDate(item['DT'])
+            item['Br'] = int(item['Br'])
+            item['Sr'] = int(item['Sr'])
+            item['Q'] = int(item['Q'])
+            item['P'] = float(item['P'])
+            
+        response['Ps']['P'] = float(response['Ps']['P'])
+        response['Ps']['TC'] = int(response['Ps']['TC'])
+        response['Ps']['TT'] = int(response['Ps']['TT'])
+
+        novos = filter(lambda x: (x['DT'] >= self.trade[response['S']].datetime_buss and (x['Br'] != self.trade[response['S']].buyer or x['Sr'] != self.trade[response['S']].seller or x['Q'] != self.trade[response['S']].qtd or x['P'] != self.trade[response['S']].price)), response['Ts'])
+        for item in sorted(novos, key=lambda x: x['DT']):        
             self.trade[response['S']].id = None
-            datetime_buss = response['UT']
-            self.trade[response['S']].datetime_buss = datetime(int(datetime_buss[0:4]), int(datetime_buss[5:7]), int(datetime_buss[8:10]), int(datetime_buss[11:13]), int(datetime_buss[14:16]), int(datetime_buss[17:19]), int(datetime_buss[20:24]), tzinfo=pytz.UTC)
-            self.trade[response['S']].price = price
-            self.trade[response['S']].business = business
-            self.trade[response['S']].tot_ctrcts_papers = tot_ctrcts_papers
+            self.trade[response['S']].datetime_buss = item['DT']
+            self.trade[response['S']].buyer = item['Br']
+            self.trade[response['S']].seller = item['Sr']
+            self.trade[response['S']].price = item['P']
+            self.trade[response['S']].qtd = item['Q']
+            self.trade[response['S']].tot_qtd = response['Ps']['TT']
+            self.trade[response['S']].tot_buss = response['Ps']['TC']
             self.trade[response['S']].save()
+        
+    def toDate(self, dtStr):
+        return datetime(int(dtStr[0:4]), int(dtStr[5:7]), int(dtStr[8:10]), int(dtStr[11:13]), int(dtStr[14:16]), int(dtStr[17:19]), int(dtStr[20:24]), tzinfo=pytz.UTC)
